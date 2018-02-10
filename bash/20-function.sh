e99d1a5156eaa40d1b397bc642b58a48c358fde3
Dmitriy Tyurin <fobia3d@gmail.com>
2018-02-10 15:54:11 +0300

    add docker helper

--- bash/20-function.sh
+++ bash/20-function.sh
@@ -82,3 +82,19 @@ _pear_cd_dir ()
 }
 alias pear=_pear_cd_dir
 
+dockerip()
+{
+    docker inspect -f '{{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}} ' "$@"
+}
+
+dockerips()
+{
+    for dock in $(docker ps|tail -n +2|cut -d" " -f1)
+    do
+        local dock_ip=$(dockerip $dock)
+        regex="s/$dock\s\{4\}/${dock:0:4}  ${dock_ip:-local.host}/g;$regex"
+    done
+    docker ps -a | sed -e "$regex"
+}
+alias _id_='docker ps -l -q'
+
