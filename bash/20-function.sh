a9f2313ff39d24afb334f60b7833b0c5d1f3b946
Dmitriy Tyurin <fobia3d@gmail.com>
2018-05-28 07:19:53 +0000

    func

--- bash/20-function.sh
+++ bash/20-function.sh
@@ -81,6 +81,22 @@ _pear_cd_dir ()
     return 0
 }
 alias pear=_pear_cd_dir
+# ---------------------
+
+function fs()
+{
+  if du -b /dev/null > /dev/null 2>&1; then
+    local arg=-sbh;
+  else
+    local arg=-sh;
+  fi
+  if [[ -n "$@" ]]; then
+    du $arg -- "$@";
+  else
+    du $arg .[^.]* ./*;
+  fi;
+}
+
 
 dockerip()
 {
